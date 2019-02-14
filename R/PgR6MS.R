791903e5920a6486c7289062e53cb1e8fec1f806
iferres <igferres@gmail.com>
2019-02-14 16:21:50 -0300

    init and basic sequence manipulation

--- R/PgR6MS.R
+++ R/PgR6MS.R
@@ -8,6 +8,7 @@
 #                                              .Names = c("gene1", "gene2"))),
 #                        .Names = c("org1", "org2", "org3"))
 
+#' @export
 PgR6MS <- R6Class('PgR6MS',
 
                   inherit = PgR6M,
@@ -21,18 +22,33 @@ PgR6MS <- R6Class('PgR6MS',
                     initialize = function(cluster_list,
                                           prefix = 'group',
                                           sequences,
-                                          sep = '___'){
+                                          sep = '__'){
 
                       super$initialize(cluster_list = cluster_list,
-                                       prefix = prefix)
+                                       prefix = prefix,
+                                       sep = sep)
 
                       # Check input sequences
                       # Expect a named list (names = organisms names), with a
                       # named vector of strings. Names of strings are gene
-                      # names.
-                      if (!all(self$organisms %in% names(sequences)))
+                      # names. Applies separator prior to check.
+                      nmsq <- names(sequences)
+                      if (!all(self$organisms %in% nmsq))
                         stop('organism names in cluster_list do not match names of sequences.')
 
+                      # Rename genes applying separator. This is to later make
+                      # them hashable.
+                      sequences <- lapply(seq_along(sequences), function(i){
+                        sq <- sequences[i]
+                        norg <- names(sq)
+                        sqv <- sq[[1]]
+                        names(sqv) <- paste(norg, sep, names(sqv), sep = '')
+                        sqv
+                      })
+                      names(sequences) <- nmsq
+
+                      # Extract for each organism the gene names in each
+                      # cluster.
                       namesInClusters <- lapply(self$organisms, function(x){
                         unlist(sapply(self$clusters, function(y){
                          rr <- y[which(names(y)==x)]
@@ -41,29 +57,23 @@ PgR6MS <- R6Class('PgR6MS',
                       })
                       names(namesInClusters) <- self$organisms
 
+
+                      # Check if gene names in clusters match gene names in
+                      # sequences. Stop if not.
                       genesOK <- all(sapply(self$organisms, function(x){
                         all(namesInClusters[[x]]%in%names(sequences[[x]]))
                       }))
                       genesOK2 <- all(sapply(self$organisms, function(x){
                         all(names(sequences[[x]]%in%namesInClusters[[x]]))
                       }))
-
                       if (!genesOK){
                         stop('gene names in sequences are not contained in gene names in cluster_list')
                       }else if (!genesOK2) {
                         warning('Not all gene names in sequences are present in cluster_list. Moving on.')
                       }
 
-                      #set new names to make them hashable in a single env
-                      sequences <- lapply(seq_along(sequences), function(i){
-                        nnam <- paste(names(sequences[i]),
-                                      names(sequences[[i]]),
-                                      sep =  sep)
-                        setNames(sequences[[i]], nnam)
-                      })
-
+                      # Create private env and populate it with sequences.
                       sequences <- unlist(sequences)
-
                       private$p_sequences <- list2env(sequences, hash = TRUE)
                     }
 
