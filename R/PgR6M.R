61a38a35b6cbfb64dec3a878f3f1c706f186dc46
iferres <igferres@gmail.com>
2019-03-27 17:08:04 -0300

    removed some old not working dependencies

--- R/PgR6M.R
+++ R/PgR6M.R
@@ -325,7 +325,7 @@
 #'
 #'
 #' @importFrom R6 R6Class
-#' @importFrom micropan distJaccard distManhattan heaps fluidity binomixEstimate
+#' @importFrom micropan distJaccard fluidity binomixEstimate
 #' @importFrom ggplot2 ggplot aes geom_bar geom_raster geom_tile theme element_blank scale_fill_grey xlab ylab coord_polar
 #' @importFrom reshape2 melt
 #' @export
