092c7f1e20f1cbad60b1b2fcb11b734ca3e0179b
iferres <igferres@gmail.com>
2019-04-29 16:54:01 -0300

    added autoplot

--- R/PgR6M.R
+++ R/PgR6M.R
@@ -417,7 +417,7 @@
 #'
 #' @importFrom R6 R6Class
 #' @importFrom micropan distJaccard fluidity binomixEstimate
-#' @importFrom ggplot2 ggplot aes geom_bar geom_raster geom_tile theme element_blank scale_fill_grey xlab ylab coord_polar stat_function
+#' @importFrom ggplot2 ggplot aes geom_bar geom_raster geom_tile theme element_blank scale_fill_grey xlab ylab coord_polar stat_function autoplot
 #' @importFrom reshape2 melt
 #' @importFrom vegan vegdist
 #' @export
