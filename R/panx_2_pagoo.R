7dcc51908288f2eadf26e263d068b87372d52391
iferres <igferres@gmail.com>
2019-08-12 16:09:54 -0300

    progress with panx_2_pagoo

---
+++ R/panx_2_pagoo.R
@@ -0,0 +1,23 @@
+#' @importFrom reshape2 melt
+#' @export
+panx_2_pagoo <- function(allclusters_final_tsv, nucleotide_fnas, group_prefix = 'group', sep = '__'){
+
+  x <- readLines(allclusters_final_tsv)
+  spl <- strsplit(x, '\t')
+  spl <- setNames(spl, paste0(group_prefix, seq_len(length(x))))
+  sb <- lapply(spl, function(i) sub('[|]', sep, i))
+  df <- melt(sb)
+  df$org <- sub(paste0(sep, '.+$'), '', df$value)
+  colnames(df) <- c('gene', 'group', 'org')
+
+  if (missing(nucleotide_fnas)){
+
+
+
+  }else{
+
+
+  }
+
+
+}
